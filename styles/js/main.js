d55559485c1c69af914048421f4977102bfeeec1
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-04-19 13:12:06 +0100

    completed

---
+++ styles/js/main.js
@@ -0,0 +1,22 @@
+checkbox.addEventListener("change", lightDarkmood);
+function lightDarkmood() {
+    document.body.classList.toggle("dark")
+//     if (localStorage.getItem("theme") == "light") {
+//         localStorage.setItem("theme", "dark");
+//     }
+//     else {
+//         localStorage.setItem("theme", "light");
+//     }
+// }
+
+
+// if (localStorage.getItem("theme") == "light") {
+//     document.body.classList.remove("dark");
+    
+// }
+// else if (localStorage.getItem("theme") == "dark") {
+//     document.body.classList.add("dark");
+// }
+// else {
+//     localStorage.setItem("theme", "light");
+}
